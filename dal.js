f208761de14d030309bcc66c8f438a103cab8d72
Kate Burnett <catherineburnett18@gmail.com>
2023-05-10 00:17:34 -0400

    uri

--- dal.js
+++ dal.js
@@ -1,33 +1,33 @@
-const MongoClient = require('mongodb');
-//.MongoClient;
+const MongoClient = require('mongodb').MongoClient;
 // const url = process.env.REACT_APP_MONGO_URI?process.env.REACT_APP_MONGO_URI:'mongodb://localhost:27017';
 // let db = null;
 
 // const { MongoClient, ServerApiVersion } = require('mongodb');
-// const uri = process.env.REACT_APP_MONGO_URI?process.env.MONGO_URI:'mongodb://localhost:27017';
+const uri = "mongodb+srv://catherineburnett18:Glasgow18@bankingcluster.kehcwyj.mongodb.net/?retryWrites=true&w=majority"
+//?process.env.MONGO_URI:'mongodb://localhost:27017';
 
-// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-// const client = new MongoClient(uri, { useUnifiedTopology: true }, {
-//   serverApi: {
-//     version: ServerApiVersion.v1,
-//     strict: true,
-//     deprecationErrors: true,
-//   }
-// });
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, { useUnifiedTopology: true }, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
 
-// async function run() {
-//   try {
-//     // Connect the client to the server	(optional starting in v4.7)
-//     await client.connect();
-//     // Send a ping to confirm a successful connection
-//     await client.db("admin").command({ ping: 1 });
-//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-//   } finally {
-//     // Ensures that the client will close when you finish/error
-//     await client.close();
-//   }
-// }
-// run().catch(console.dir);
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
 
 
 // // connect to mongo
@@ -38,30 +38,30 @@ const MongoClient = require('mongodb');
 //     db = client.db('myproject');
 // });
 
-async function main(){
-    /**
-     * Connection URI.
-     */
-    const uri = process.env.REACT_APP_MONGO_URI?process.env.MONGO_URI:'mongodb://localhost:27017';
+// async function main(){
+//     /**
+//      * Connection URI.
+//      */
+//     const uri = process.env.REACT_APP_MONGO_URI?process.env.MONGO_URI:'mongodb://localhost:27017';
  
 
-    const client = new MongoClient(uri);
+//     const client = new MongoClient(uri);
  
-    try {
-        // Connect to the MongoDB cluster
-        await client.connect();
+//     try {
+//         // Connect to the MongoDB cluster
+//         await client.connect();
  
-        // Make the appropriate DB calls
-        await  listDatabases(client);
+//         // Make the appropriate DB calls
+//         await  listDatabases(client);
  
-    } catch (e) {
-        console.error(e);
-    } finally {
-        await client.close();
-    }
-}
+//     } catch (e) {
+//         console.error(e);
+//     } finally {
+//         await client.close();
+//     }
+// }
 
-main().catch(console.error);
+// main().catch(console.error);
 
 
 // create user account using the collection.insertOne function
