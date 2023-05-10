6c3417e4ead554b14abb39895c632a71dd89fa37
Kate Burnett <catherineburnett18@gmail.com>
2023-05-09 23:33:02 -0400

    uri

--- dal.js
+++ dal.js
@@ -1,41 +1,41 @@
-// const MongoClient = require('mongodb').MongoClient;
-// const url = process.env.MONGO_URI?process.env.MONGO_URI:'mongodb://localhost:27017';
-// let db = null;
+const MongoClient = require('mongodb').MongoClient;
+const url = process.env.REACT_APP_MONGO_URI?process.env.MONGO_URI:'mongodb://localhost:27017';
+let db = null;
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = process.env.REACT_APP_MONGO_URI?process.env.MONGO_URI:'mongodb://localhost:27017';
+// const { MongoClient, ServerApiVersion } = require('mongodb');
+// const uri = process.env.REACT_APP_MONGO_URI?process.env.MONGO_URI:'mongodb://localhost:27017';
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, { useUnifiedTopology: true }, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
-
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
-
-
-// // connect to mongo
-// MongoClient.connect(url, { useUnifiedTopology: true }, function (err, client) {
-//     console.log("Connected successfully to db server");
-
-//     // connect to myproject database
-//     db = client.db('myproject');
+// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// const client = new MongoClient(uri, { useUnifiedTopology: true }, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   }
 // });
+
+// async function run() {
+//   try {
+//     // Connect the client to the server	(optional starting in v4.7)
+//     await client.connect();
+//     // Send a ping to confirm a successful connection
+//     await client.db("admin").command({ ping: 1 });
+//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+//   } finally {
+//     // Ensures that the client will close when you finish/error
+//     await client.close();
+//   }
+// }
+// run().catch(console.dir);
+
+
+// connect to mongo
+MongoClient.connect(url, { useUnifiedTopology: true }, function (err, client) {
+    console.log("Connected successfully to db server");
+
+    // connect to myproject database
+    db = client.db('myproject');
+});
 
 // create user account using the collection.insertOne function
 function create(name, email, password) {
