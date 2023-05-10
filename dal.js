01f8206e372c14d993749b1cdc448417af468f3e
Kate Burnett <catherineburnett18@gmail.com>
2023-05-09 23:10:34 -0400

    add .env

--- dal.js
+++ dal.js
@@ -1,18 +1,45 @@
-const MongoClient = require('mongodb').MongoClient;
-const url = process.env.MONGO_URI ? process.env.MONGO_URI:'mongodb://localhost:27017';
-let db = null;
+// const MongoClient = require('mongodb').MongoClient;
+// const url = process.env.MONGO_URI?process.env.MONGO_URI:'mongodb://localhost:27017';
+// let db = null;
 
-// connect to mongo
-MongoClient.connect(url, { useUnifiedTopology: true }, function (err, client) {
-    console.log("Connected successfully to db server");
 
-    // connect to myproject database
-    db = client.db('myproject');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://catherineburnett18:<password>@bankingcluster.kehcwyj.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
 });
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+// // connect to mongo
+// MongoClient.connect(url, { useUnifiedTopology: true }, function (err, client) {
+//     console.log("Connected successfully to db server");
+
+//     // connect to myproject database
+//     db = client.db('myproject');
+// });
 
 // create user account using the collection.insertOne function
 function create(name, email, password) {
-    // TODO: populate this function based off the video
     return new Promise ((resolve, reject) => {
         const collection = db.collection('users');
         const doc = {name, email, password, balance: 0};
